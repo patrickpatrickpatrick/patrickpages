1bf6ebf8fc07ed413d14e0595816e763ccd100ce
Patrick Cartlidge <patrickcartlidge@gmail.com>
2024-01-04 11:33:59 +0000

    get google secrets working i guess

---
+++ mailer-script.js
@@ -0,0 +1,41 @@
+import { initializeApp, applicationDefault } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
+let firebaseConfig;
+
+if (process.env.GOOGLE_CREDS) {
+  firebaseConfig = JSON.parse(process.env.GOOGLE_CREDS);
+
+  console.log('auth works')
+
+  return "Success"
+}
+// } else {
+//   firebaseConfig = applicationDefault();
+// }
+
+// const app = initializeApp(firebaseConfig);
+
+// export const db = getFirestore(app);
+
+// export const getData = async (ref, query) => {
+
+//   const snapshot = await ref.where(...query).get()
+
+//   if (snapshot.empty) {
+//     console.log('No matching documents.');
+//     return {
+//       results: []
+//     };
+//   }
+
+//   let results = [];
+
+//   snapshot.forEach(doc => {
+//     results = [ ...results, doc.data() ]
+//   });
+
+//   return {
+//     results
+//   }
+// }
