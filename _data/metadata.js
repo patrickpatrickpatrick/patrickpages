f9a02f391e86144c2c5a8fbbdb54f6f1db8a8c8c
Patrick <patrickcartlidge@gmail.com>
2025-12-30 13:08:35 +0000

    use 11ty modern

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
-module.exports = {
-	title: "Patrick's Pages",
+export default {
+	title: "Eleventy Base Blog v9",
 	url: "https://example.com/",
 	language: "en",
-	description: "It's Patrick's website",
+	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Patrick",
-		email: "",
-		url: "https://patrickpatrickpatrick.github.io/about/"
+		name: "Your Name Here",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/"
 	}
 }
